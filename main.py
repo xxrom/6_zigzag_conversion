5fec098018e0d629311477c972f209ff373775e7
Chernyshov_Nikita <xpom5566@mail.ru>
2020-08-27 01:49:58 +0300

    solved: 36ms (99.99%) !!!!!!!!!!!!!! / 13.8 (79.7%) really cool =) so fast !

--- main.py
+++ main.py
@@ -1,40 +1,19 @@
 class Solution:
   def convert(self, s: str, n: int) -> str:
-    ans = ''
-    allLevels = []
+    allLevels = [''] * n
 
-    for i in range(n):
-      allLevels.append([])
-
-    # print(allLevels)
-    size = len(s)
-    i = 0
-    inc = +1
+    inc = +1 if n > 1 else 0
     level = 0
-    count = 0
 
-    while i < size:
-      char = s[i]
-      print('char %s [%d]' % (char, i))
+    for char in s:
+      allLevels[level]  += char
 
-      allLevels[level] += char
-      # print(allLevels)
-
-      i += 1
       level += inc
 
-      if (level == n - 1):
+      if level == n - 1 or level == 0:
         inc *= -1
 
-      if (level == 0):
-        inc *= -1
-
-
-    # print(allLevels)
-    for i in range(n):
-      ans += ''.join(allLevels[i])
-
-    return ans
+    return ''.join(allLevels)
 
 my = Solution()
 
@@ -45,5 +24,5 @@ trueAns= "PAHNAPLSIIGYIR"
 ans = my.convert(s, n)
 print("ans", ans, ans == trueAns)
 
-# Runtime: 64 ms, faster than 72.25% of Python3 online submissions for ZigZag Conversion.
-# Memory Usage: 14 MB, less than 20.94% of Python3 online submissions for ZigZag Conversion.
+# Runtime: 36 ms, faster than 99.99% of Python3 online submissions for ZigZag Conversion.
+# Memory Usage: 13.8 MB, less than 79.72% of Python3 online submissions for ZigZag Conversion.
